fce02c22fdccbe9a9138a9790027596e452981b0
ruohuaii <bfdbfsnl@github.com>
2023-10-18 09:38:41 +0800

    feat:增加获取token方法

--- model/channel.go
+++ model/channel.go
@@ -24,3 +24,26 @@ func (r *CreatePayPalChannelRes) Ok() bool {
 func (r *CreatePayPalChannelRes) Error() error {
 	return fmt.Errorf(r.Msg)
 }
+
+type (
+	CreateWechatSubChannelReq struct {
+		ZPayMchID   uint64 `json:"zpay_mch_id"`
+		ChannelID   uint64 `json:"channel_id"`
+		WechatMchID string `json:"wechat_mch_id"`
+	}
+	CreateWechatSubChannelRes struct {
+		Code int    `json:"code"`
+		Msg  string `json:"msg"`
+		Data struct {
+			ChannelID uint64 `json:"channel_id"`
+		}
+	}
+)
+
+func (r *CreateWechatSubChannelRes) Ok() bool {
+	return r.Code == 0
+}
+
+func (r *CreateWechatSubChannelRes) Error() error {
+	return fmt.Errorf(r.Msg)
+}
