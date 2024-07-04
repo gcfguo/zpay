018952758d087b8ec6265a8210cbcfbec55242a6
ruohuaii <bfdbfsnl@github.com>
2024-07-04 13:20:14 +0800

    feat:增加物流上传API

--- client.go
+++ client.go
@@ -72,8 +72,7 @@ func (c *Client) newRequest(
 func (c *Client) doRequest(
 	httpMethod string,
 	reqURI string,
-	data any,
-	scanner any) (*string, error) {
+	data any) (*string, error) {
 	req, err := c.newRequest(httpMethod, reqURI, data)
 	if err != nil {
 		return nil, err
@@ -81,47 +80,31 @@ func (c *Client) doRequest(
 
 	var resp *http.Response
 	for t := 0; t < 3; t++ {
-		resp, err = c.httpClient.Do(req)
-		if err == nil && resp != nil {
+		resp, _ = c.httpClient.Do(req)
+		if resp != nil {
 			break
 		}
-
-		if _, ok := err.(net.Error); ok {
+		var err net.Error
+		if errors.As(err, &err) {
 			continue
 		}
-
-		return nil, err
 	}
 	if resp == nil {
 		return nil, fmt.Errorf("too many failures")
 	}
-
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 	_ = resp.Body.Close()
 	content := string(b)
-	if scanner == nil {
-		return &content, nil
-	}
-	var result APIResult
-	result.Data = scanner
-	err = json.Unmarshal(b, &result)
-	if err != nil {
-		return &content, err
-	}
-	if result.Code != 0 {
-		return &content, fmt.Errorf(result.Msg)
-	}
 	return &content, err
 }
 
 func (c *Client) doRequestWithToken(
 	httpMethod string,
 	reqURI string,
-	data any,
-	scanner any) (*string, error) {
+	data any) (*string, error) {
 	err := c.check()
 	if err != nil {
 		return nil, err
@@ -142,31 +125,24 @@ func (c *Client) doRequestWithToken(
 
 	var resp *http.Response
 	for t := 0; t < 3; t++ {
-		resp, err = c.httpClient.Do(req)
-		if err == nil && resp != nil {
+		resp, _ = c.httpClient.Do(req)
+		if resp != nil {
 			break
 		}
-
-		if _, ok := err.(net.Error); ok {
+		var err net.Error
+		if errors.As(err, &err) {
 			continue
 		}
-
-		return nil, err
 	}
 	if resp == nil {
 		return nil, fmt.Errorf("too many failures")
 	}
-
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 	_ = resp.Body.Close()
 	content := string(b)
-	if scanner == nil {
-		return &content, nil
-	}
-	err = json.Unmarshal(b, scanner)
 	return &content, err
 }
 
@@ -207,7 +183,7 @@ func (c *Client) GetAccessToken() string {
 // Register
 // 注册
 func (c *Client) Register(req *model.RegisterReq) (*model.RegisterRes, error) {
-	resContent, err := c.doRequest(http.MethodPost, "/v1/open/merchant/register", req, nil)
+	resContent, err := c.doRequest(http.MethodPost, "/v1/open/merchant/register", req)
 	if err != nil {
 		return nil, err
 	}
@@ -222,7 +198,7 @@ func (c *Client) Register(req *model.RegisterReq) (*model.RegisterRes, error) {
 // Login
 // 登录
 func (c *Client) Login(req *model.LoginReq) (*model.LoginRes, error) {
-	resContent, err := c.doRequest(http.MethodPost, "/v1/open/merchant/login", req, nil)
+	resContent, err := c.doRequest(http.MethodPost, "/v1/open/merchant/login", req)
 	if err != nil {
 		return nil, err
 	}
@@ -237,7 +213,7 @@ func (c *Client) Login(req *model.LoginReq) (*model.LoginRes, error) {
 // GetApps
 // 获取应用
 func (c *Client) GetApps(req *model.GetAppsReq) (*model.GetAppsRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodGet, "/v1/api/app/get", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodGet, "/v1/api/app/get", req)
 	if err != nil {
 		return nil, err
 	}
@@ -252,7 +228,7 @@ func (c *Client) GetApps(req *model.GetAppsReq) (*model.GetAppsRes, error) {
 // CreateApp
 // 创建应用
 func (c *Client) CreateApp(req *model.CreateAppReq) (*model.CreateAppRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/app/create", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/app/create", req)
 	if err != nil {
 		return nil, err
 	}
@@ -267,7 +243,7 @@ func (c *Client) CreateApp(req *model.CreateAppReq) (*model.CreateAppRes, error)
 // AppSettings
 // 应用设置
 func (c *Client) AppSettings(req *model.AppSettingsReq) (*model.AppSettingsRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/app/settings", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/app/settings", req)
 	if err != nil {
 		return nil, err
 	}
@@ -282,7 +258,7 @@ func (c *Client) AppSettings(req *model.AppSettingsReq) (*model.AppSettingsRes, 
 // CreatePayPalChannel
 // 创建paypal支付渠道
 func (c *Client) CreatePayPalChannel(req *model.CreatePayPalChannelReq) (*model.CreatePayPalChannelRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/paypal/create", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/paypal/create", req)
 	if err != nil {
 		return nil, err
 	}
@@ -297,7 +273,7 @@ func (c *Client) CreatePayPalChannel(req *model.CreatePayPalChannelReq) (*model.
 // CreateWechatChannel
 // 创建微信支付渠道
 func (c *Client) CreateWechatChannel(req *model.CreateWechatSubChannelReq) (*model.CreateWechatSubChannelRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/wechat/sub/create", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/wechat/sub/create", req)
 	if err != nil {
 		return nil, err
 	}
@@ -312,7 +288,7 @@ func (c *Client) CreateWechatChannel(req *model.CreateWechatSubChannelReq) (*mod
 // CreatePayWay
 // 创建支付方式
 func (c *Client) CreatePayWay(req *model.CreatePayWayReq) (*model.CreatePayWayRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/payway/create", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/payway/create", req)
 	if err != nil {
 		return nil, err
 	}
@@ -327,7 +303,7 @@ func (c *Client) CreatePayWay(req *model.CreatePayWayReq) (*model.CreatePayWayRe
 // Payment
 // 支付
 func (c *Client) Payment(req *model.PaymentReq) (*model.PaymentRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/order/payment", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/order/payment", req)
 	if err != nil {
 		return nil, err
 	}
@@ -342,7 +318,7 @@ func (c *Client) Payment(req *model.PaymentReq) (*model.PaymentRes, error) {
 // Refund
 // 退款
 func (c *Client) Refund(req *model.RefundReq) (*model.RefundRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/order/refund", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/order/refund", req)
 	if err != nil {
 		return nil, err
 	}
@@ -358,7 +334,7 @@ func (c *Client) Refund(req *model.RefundReq) (*model.RefundRes, error) {
 // 获取支付方式
 func (c *Client) GetPayWays(req *model.GetPayWaysReq) (
 	*model.GetPayWaysRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/payway/get", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/payway/get", req)
 	if err != nil {
 		return nil, err
 	}
@@ -372,7 +348,7 @@ func (c *Client) GetPayWays(req *model.GetPayWaysReq) (
 
 func (c *Client) AddPayWay(req *model.AddPayWayReq) (
 	*model.AddPayWayRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/payway/add", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/payway/add", req)
 	if err != nil {
 		return nil, err
 	}
@@ -388,7 +364,7 @@ func (c *Client) AddPayWay(req *model.AddPayWayReq) (
 // 订单推送海关
 func (c *Client) CustomsDeclare(req *model.CustomsDeclareReq) (
 	*model.CustomsDeclareRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/customs/declare", req, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/customs/declare", req)
 	if err != nil {
 		return nil, err
 	}
@@ -415,11 +391,24 @@ func (c *Client) CustomsQuery() {
 // ShowPaymentChannel
 // 展示支付渠道
 func (c *Client) ShowPaymentChannel() (*model.ShowPaymentChannelRes, error) {
-	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/payway/channels", nil, nil)
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/payway/channels", nil)
 	if err != nil {
 		return nil, err
 	}
 	res := new(model.ShowPaymentChannelRes)
+	err = c.handleResponse([]byte(*resContent), res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *Client) LogisticsUpload(req *model.LogisticsUploadReq) (*model.LogisticsUploadRes, error) {
+	resContent, err := c.doRequestWithToken(http.MethodPost, "/v1/api/order/logistics/upload", req)
+	if err != nil {
+		return nil, err
+	}
+	res := new(model.LogisticsUploadRes)
 	err = c.handleResponse([]byte(*resContent), res)
 	if err != nil {
 		return nil, err
